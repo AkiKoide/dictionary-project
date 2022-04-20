025685ee41df77cb8ae106e587f3b049dedb6ec4
AkiKoide <akikoide924@gmail.com>
2022-04-20 17:14:13 +0200

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from "./logo-aki.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div classname="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">
+          <small>coded by Aki Koide</small>
+        </footer>
+      </div>
     </div>
   );
 }
