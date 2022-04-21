e9f49a7db0c7e836f0f8fcbad6703533aca05199
AkiKoide <akikoide924@gmail.com>
2022-04-21 16:58:47 +0200

    added meaning definitions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      <div classname="container">
+      <div className="container">
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
