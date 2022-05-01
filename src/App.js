01b55be885731929f279606701f60b2fdd0a5abf
AkiKoide <akikoide924@gmail.com>
2022-05-01 17:17:10 +0200

    added font awesome

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo-aki.png";
+import logo from "./titlelogo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -7,14 +7,42 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <img src={logo} className="App-logo" class="img-fluid" alt="logo" />
         </header>
         <main>
-          <Dictionary defaultKeyword="hello" />
+          <Dictionary defaultKeyword="Japan" />
         </main>
 
         <footer className="App-footer">
-          <small>coded by Aki Koide</small>
+          <small>
+            This project was created by Aki Koide and is{" "}
+            <a
+              href="https://github.com/AkiKoide/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on github
+            </a>
+          </small>
+          <br />
+          <span>
+            <a
+              href="https://github.com/AkiKoide"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="fa-brands fa-github fa-xl"></i>
+            </a>
+          </span>{" "}
+          <span>
+            <a
+              href="https://www.linkedin.com/in/aki-koide/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="fa-brands fa-linkedin fa-xl"></i>
+            </a>
+          </span>
         </footer>
       </div>
     </div>
